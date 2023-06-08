afc9902bf1a2b18e2f91531ee1c875b401756cfb
foundationer <foundationer@tuta.io>
2023-06-08 19:48:44 -0300

    Skeleton project!

---
+++ src/main/java/org/phase5/idealoctotribble/IdealOctoTribbleApplication.java
@@ -0,0 +1,13 @@
+package org.phase5.idealoctotribble;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdealOctoTribbleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdealOctoTribbleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/phase5/idealoctotribble/IdealOctoTribbleApplicationTests.java
@@ -0,0 +1,13 @@
+package org.phase5.idealoctotribble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdealOctoTribbleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
