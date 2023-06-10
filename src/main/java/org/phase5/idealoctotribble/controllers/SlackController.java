579d2996447b62dd0eef0ecc7e3b8f5ccee23aaa
foundationer <foundationer@tuta.io>
2023-06-10 10:07:42 -0300

    Created hello world bot.

---
+++ src/main/java/org/phase5/idealoctotribble/controllers/SlackController.java
@@ -0,0 +1,14 @@
+package org.phase5.idealoctotribble.controllers;
+
+import com.slack.api.bolt.App;
+import com.slack.api.bolt.servlet.SlackAppServlet;
+import javax.servlet.annotation.WebServlet;
+
+
+@WebServlet("/slack/events")
+public class SlackController extends SlackAppServlet {
+
+    public SlackController(App app) {
+        super(app);
+    }
+}
